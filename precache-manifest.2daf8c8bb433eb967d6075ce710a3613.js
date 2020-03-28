ef031e670aa29ed2902ed7f6578925b17b8f5977
Mackenzie Salloum <msalloum@tveon.com>
2020-03-28 13:41:14 -0700

    Updates

---
+++ precache-manifest.2daf8c8bb433eb967d6075ce710a3613.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d71c6068a6153f21d007f7477dbc9c0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0b7458d291849cfe7ad4",
+    "url": "/static/js/2.2316c0a8.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.2316c0a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a4eb5756973e8e1138d",
+    "url": "/static/js/main.362ea046.chunk.js"
+  },
+  {
+    "revision": "0b4f3269348c2e5315e5",
+    "url": "/static/js/runtime-main.9089ee33.js"
+  },
+  {
+    "revision": "9942588a6c84f959132556d99e83ded6",
+    "url": "/static/media/Raleway-Regular.9942588a.ttf"
+  },
+  {
+    "revision": "4b15e239aa495e365e540acf71843b74",
+    "url": "/static/media/sarah-crutchfield-Dq3LFhdmYcw-unsplash.4b15e239.jpg"
+  }
+]);
