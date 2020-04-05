c81d6e2782093820fcce1488e8896c8944587145
Mackenzie Salloum <mackenziesalloum@gmail.com>
2020-04-05 13:18:41 -0700

    Improve responsiveness with breakpoints

--- src/Site.js
+++ src/Site.js
@@ -1,11 +1,11 @@
 import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
-  Container,
   Grid,
   IconButton,
   Link,
   Typography,
+  useMediaQuery,
 } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import React from "react";
@@ -64,22 +64,40 @@ const useStyles = makeStyles((theme) => ({
     padding: 5,
     border: `1px solid ${theme.palette.text.secondary}`,
   },
+  removeChildSidePadding: {
+    "& > .MuiGrid-item": {
+      paddingRight: 0,
+      paddingLeft: 0,
+    },
+  },
 }));
 
 function Site() {
   const classes = useStyles(theme);
+  const isSmall = useMediaQuery(theme.breakpoints.only("sm"));
+  const atLeastMed = useMediaQuery(theme.breakpoints.up("md"));
 
   return (
     <div className={classes.mainImage}>
       <div className={classes.darken}>
-        <Container className={classes.fullHeight}>
+        <Grid
+          className={classes.fullHeight}
+          container
+          direction="column"
+          justify="center"
+          alignItems="center"
+        >
           <Grid
             container
             direction="column"
             justify="center"
             alignItems="center"
+            wrap="nowrap"
             spacing={3}
-            className={classes.fullHeight}
+            xs={7}
+            sm={7}
+            md={12}
+            className={classes.removeChildSidePadding}
           >
             <Grid item>
               <div className={classes.profileImage} />
@@ -96,15 +114,59 @@ function Site() {
                 Mackenzie Salloum
               </Typography>
             </Grid>
-            <Grid item>
-              <Typography
-                variant="h5"
-                align="center"
-                style={{ color: theme.palette.text.secondary }}
-              >
-                Software Developer | Employed at{" "}
-                <Link href={currentCompanyLink}>{currentCompanyText}</Link>
-              </Typography>
+            <Grid
+              item
+              container
+              justify="center"
+              alignItems="center"
+              spacing={1}
+            >
+              <Grid item xs={12} md="auto">
+                <Typography
+                  variant="h5"
+                  align={isSmall ? "left" : "center"}
+                  style={{ color: theme.palette.text.secondary }}
+                >
+                  Software Developer
+                </Typography>
+              </Grid>
+              <Grid item xs={12} sm="auto">
+                <Typography
+                  variant="h5"
+                  align="center"
+                  style={{ color: theme.palette.text.secondary }}
+                >
+                  {atLeastMed && "\u00A0\u00A0|\u00A0\u00A0"}
+                </Typography>
+              </Grid>
+              <Grid item xs={12} md="auto">
+                <Typography
+                  variant="h5"
+                  align="center"
+                  style={{ color: theme.palette.text.secondary }}
+                >
+                  ReactJS Enthusiast
+                </Typography>
+              </Grid>
+              <Grid item xs={12} sm="auto">
+                <Typography
+                  variant="h5"
+                  align="center"
+                  style={{ color: theme.palette.text.secondary }}
+                >
+                  {atLeastMed && "\u00A0\u00A0|\u00A0\u00A0"}
+                </Typography>
+              </Grid>
+              <Grid item xs={12} md="auto">
+                <Typography
+                  variant="h5"
+                  align={isSmall ? "right" : "center"}
+                  style={{ color: theme.palette.text.secondary }}
+                >
+                  Employed at{" "}
+                  <Link href={currentCompanyLink}>{currentCompanyText}</Link>
+                </Typography>
+              </Grid>
             </Grid>
             <Grid item container justify="center" spacing={5}>
               <Grid item>
@@ -123,7 +185,7 @@ function Site() {
               </Grid>
             </Grid>
           </Grid>
-        </Container>
+        </Grid>
       </div>
     </div>
   );
