9af9095184e710f7f2bcbc5f5b9fc50abcd55af2
Mackenzie Salloum <mackenziesalloum@gmail.com>
2023-02-04 00:06:45 -0800

    Add article creation

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,8 @@
 class Article < ApplicationRecord
+  validates :title, presence: true
+  validates :body, presence: true, length: { minimum: 10 }
+
+  def created_date
+    created_at.localtime.strftime '%-d/%-m/%Y'
+  end
 end
