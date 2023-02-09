f21b3ce3829b0ea6dda0c431ae9ba8dd588f948c
Mackenzie Salloum <mackenziesalloum@gmail.com>
2023-02-08 19:36:50 -0800

    Fix migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_04_064805) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_09_033423) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "status"
   end
 
 end
