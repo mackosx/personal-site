71d6e12d8e927d0acd3a90e7618c6bf0d10cf283
Mackenzie Salloum <mackenziesalloum@gmail.com>
2023-02-20 17:41:04 -0800

    Add nav bar with home page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def cp(target_path, current_path)
+    'current' if current_page?(target_path) || (current_path.start_with?(target_path) && target_path != '/')
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  # Defines the root path route ("/")
-  root 'articles#index'
+  # Nest blog-related things under /blog
   scope '/blog' do
+    root to: 'articles#index', as: :blog
     resources :articles
   end
+  # Defines the root path route ("/")
+  root to: 'home#index'
 end
