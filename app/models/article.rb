daee7680bb2660fc1588f07cfb7d6ce34e67db95
Mackenzie Salloum <mackenziesalloum@gmail.com>
2023-03-03 16:05:27 -0800

    Integrate editor

--- app/models/article.rb
+++ app/models/article.rb
@@ -4,6 +4,8 @@ class Article < ApplicationRecord
   validates :title, presence: true
   validates :body, presence: true, length: { minimum: 10 }
 
+  has_rich_text :body
+
   def created_date
     created_at.localtime.strftime '%-d/%-m/%Y'
   end
